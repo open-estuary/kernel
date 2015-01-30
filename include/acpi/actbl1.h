1c2e7723d391f84a2b6a0e388fa1b1553f3dd27f
Tomasz Nowicki <tomasz.nowicki@linaro.org>
2015-01-30 16:56:30 +0800

    ACPI / ACPICA: Support for GICv3 ITS subtable in ACPICA code.
    This commit is not going to hit upstream, those should be provided as
    ACPICA release code. Only for test purposes.
    
    Signed-off-by: Tomasz Nowicki <tomasz.nowicki@linaro.org>
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Signed-off-by: Ding Tianhong <dingtianhong@huawei.com>

--- include/acpi/actbl1.h
+++ include/acpi/actbl1.h
@@ -673,7 +673,8 @@ enum acpi_madt_type {
 	ACPI_MADT_TYPE_GENERIC_DISTRIBUTOR = 12,
 	ACPI_MADT_TYPE_GENERIC_MSI_FRAME = 13,
 	ACPI_MADT_TYPE_GENERIC_REDISTRIBUTOR = 14,
-	ACPI_MADT_TYPE_RESERVED = 15	/* 15 and greater are reserved */
+	ACPI_MADT_TYPE_GENERIC_ITS = 15,
+	ACPI_MADT_TYPE_RESERVED = 16	/* 15 and greater are reserved */
 };
 
 /*
@@ -853,6 +854,16 @@ struct acpi_madt_generic_redistributor {
 	u16 reserved;		/* reserved - must be zero */
 	u64 base_address;
 	u32 length;
+};
+
+/* 15: Generic Redistributor (ACPI 5.2) */
+
+struct acpi_madt_generic_its {
+	struct acpi_subtable_header header;
+	u16 reserved;		/* reserved - must be zero */
+	u32 its_id;
+	u64 base_address;
+	u32 reserved2;		/* reserved2 - must be zero */
 };
 
 /*
