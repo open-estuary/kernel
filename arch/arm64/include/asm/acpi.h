5d518c5c8deae3b78bdbd46b75411fb92a7af7ab
Tomasz Nowicki <tomasz.nowicki@linaro.org>
2015-08-05 22:10:36 +0800

    acpi, apei, arm64: APEI initial support for aarch64.
    This commit provides APEI arch-specific bits for aarch64
    
    Meanwhile, add a new subfunction "hest_ia_init" for
    "acpi_disable_cmcff" which is used by IA-32 Architecture
    Corrected Machine Check (CMC), and add "HAVE_ACPI_APEI_HEST_IA32"
    to make sure only x86 use "hest_ia_init".
    
    Signed-off-by: Tomasz Nowicki <tomasz.nowicki@linaro.org>
    Tested-by: Jonathan (Zhixiong) Zhang <zjzhang@codeaurora.org>
    Signed-off-by: Fu Wei <fu.wei@linaro.org>
    Acked-by: Hanjun Guo <hanjun.guo@linaro.org>

--- arch/arm64/include/asm/acpi.h
+++ arch/arm64/include/asm/acpi.h
@@ -17,6 +17,7 @@
 
 #include <asm/cputype.h>
 #include <asm/smp_plat.h>
+#include <asm/tlbflush.h>
 
 /* Macros for consistency checks of the GICC subtable of MADT */
 #define ACPI_MADT_GICC_LENGTH	\
@@ -94,14 +95,10 @@ static inline const char *acpi_get_enable_method(int cpu)
 
 #ifdef	CONFIG_ACPI_APEI
 pgprot_t arch_apei_get_mem_attribute(phys_addr_t addr);
-#endif
 
-#ifdef CONFIG_ACPI_NUMA
-int arm64_acpi_numa_init(void);
-void acpi_numa_set_node_info(unsigned int cpu, u64 hwid);
-#else
-static inline int arm64_acpi_numa_init(void) { return -ENODEV; }
-static inline void acpi_numa_set_node_info(unsigned int cpu, u64 hwid) { }
-#endif /* CONFIG_ACPI_NUMA */
-
+static inline void arch_apei_flush_tlb_one(unsigned long addr)
+{
+	flush_tlb_kernel_range(addr, addr + PAGE_SIZE);
+}
+#endif /* CONFIG_ACPI_APEI */
 #endif /*_ASM_ACPI_H*/
