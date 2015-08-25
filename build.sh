04fabf8de0df1251858ab29a83e841aad45693f6
justin.zhao <zhaojunhua@hisilicon.com>
2015-08-25 13:13:07 +0800

    Add build and progress scripts
    Signed-off-by: justin.zhao <zhaojunhua@hisilicon.com>

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#description: build kernel.
+#author: Justin Zhao
+#date: 2015-5-29
+
+export ARCH=arm64
+march=`uname -m`
+if [ "$march" != "aarch64" ]; then
+	export CROSS_COMPILE=aarch64-linux-gnu-
+fi
+
+sudo make mrproper
+#cp config.dbg arch/arm64/configs/defconfig
+make defconfig
+
+rm *.out
+make Image -j16 > build0.out 2>&1
+
+if [ "$march" = "aarch64" ]; then
+	sudo make modules -j16 > build1.out 2>&1
+	sudo make modules_install > build2.out 2>&1
+	sudo make firmware_install > build3.out 2>&1
+fi
+
+make hisilicon/hip05-d02.dtb
+
+sudo cp /boot/Image /boot/Image.bak
+sudo cp /boot/hip05-d02.dtb /boot/hip05-d02.dtb.bak
+sudo cp arch/arm64/boot/Image /boot/
+sudo cp arch/arm64/boot/dts/hisilicon/hip05-d02.dtb /boot/
+
+find . -name Image
+find . -name hip05-d02.dtb
