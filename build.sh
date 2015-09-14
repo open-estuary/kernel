dd92dbf61d912aa0a363d65c3ea48e5c109b5df6
justin.zhao <zhaojunhua@hisilicon.com>
2015-09-14 11:07:53 +0800

    Improve build.sh to support arm32 native building
    Signed-off-by: justin.zhao <zhaojunhua@hisilicon.com>

--- build.sh
+++ build.sh
@@ -8,26 +8,47 @@ march=`uname -m`
 if [ "$march" != "aarch64" ]; then
 	export CROSS_COMPILE=aarch64-linux-gnu-
 fi
+if [ "$march" = "arm" ]; then
+    export ARCH=arm
+	export CROSS_COMPILE=
+fi
 
+rm *.out 2>/dev/null
 sudo make mrproper
-#cp config.dbg arch/arm64/configs/defconfig
-make defconfig
 
-rm *.out
-make Image -j14 > build0.out 2>&1
+if [ "$march" = "arm" ]; then
+    export ARCH=arm
+    make hisi_defconfig
+    make zImage -j14 > build0.out 2>&1
+    make hip04-d01.dtb
+    cat arch/arm/boot/zImage arch/arm/boot/dts/hip04-d01.dtb >.kernel
+else
+    make defconfig
+    make Image -j36 > build0.out 2>&1
+    make hisilicon/hip05-d02.dtb
+fi
 
-if [ "$march" = "aarch64" ]; then
+if [ "$march" = "aarch64" -o "$march" = "arm" ]; then
 	sudo make modules -j14 > build1.out 2>&1
 	sudo make modules_install -j14 > build2.out 2>&1
 	sudo make firmware_install -j14 > build3.out 2>&1
 fi
 
-make hisilicon/hip05-d02.dtb
+if [ "$march" = "arm" ]; then
+    sudo cp arch/arm/boot/zImage /boot/
+    sudo cp arch/arm/boot/dts/hip04-d01.dtb /boot/
 
-sudo cp /boot/Image /boot/Image.bak
-sudo cp /boot/hip05-d02.dtb /boot/hip05-d02.dtb.bak
-sudo cp arch/arm64/boot/Image /boot/
-sudo cp arch/arm64/boot/dts/hisilicon/hip05-d02.dtb /boot/
+    find . -name zImage
+    find . -name hip04-d01.dtb
+else
+    sudo cp /boot/Image /boot/Image.bak
+    sudo cp /boot/hip05-d02.dtb /boot/hip05-d02.dtb.bak
+    sudo cp arch/arm64/boot/Image /boot/
+    sudo cp arch/arm64/boot/dts/hisilicon/hip05-d02.dtb /boot/
 
-find . -name Image
-find . -name hip05-d02.dtb
+    scp arch/arm64/boot/Image justin@192.168.1.107:~/ftp/
+    scp arch/arm64/boot/dts/hisilicon/hip05-d02.dtb justin@192.168.1.107:~/ftp/
+
+    find . -name Image
+    find . -name hip05-d02.dtb
+fi
