01994d40d12a3b5650d7a6fdbfdfd0136e1fb14f
Hanjun Guo <hanjun.guo@linaro.org>
2015-12-12 00:06:27 +0800

    ACPI: bus: move acpi_match_device_ids() to linux/acpi.h
    acpi_match_device_ids() will be used for drivers to match
    diferent hardware versions, it will be compiled in non-ACPI
    case, but acpi_match_device_ids() in acpi_bus.h and it can
    only be used in ACPI case, so move it to linux/acpi.h and
    introduce a stub function for it.
    
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Signed-off-by: Xinwei Kong <kong.kongxinwei@hisilicon.com>

--- include/acpi/acpi_bus.h
+++ include/acpi/acpi_bus.h
@@ -511,8 +511,6 @@ void acpi_bus_unregister_driver(struct acpi_driver *driver);
 int acpi_bus_scan(acpi_handle handle);
 void acpi_bus_trim(struct acpi_device *start);
 acpi_status acpi_bus_get_ejd(acpi_handle handle, acpi_handle * ejd);
-int acpi_match_device_ids(struct acpi_device *device,
-			  const struct acpi_device_id *ids);
 int acpi_create_dir(struct acpi_device *);
 void acpi_remove_dir(struct acpi_device *);
 
