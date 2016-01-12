904692398bdc661e56cbf375634f784e3d23fd3e
Tomasz Nowicki <tn@semihalf.com>
2016-01-12 18:46:23 +0100

    arm64, pci, acpi: Start using ACPI based PCI host bridge driver for ARM64.
    Lets get rid of empty PCI init stub, related ACPI header and
    go with full-blown PCI host bridge driver.
    
    Signed-off-by: Tomasz Nowicki <tn@semihalf.com>
    CC: Arnd Bergmann <arnd@arndb.de>
    CC: Catalin Marinas <catalin.marinas@arm.com>
    CC: Liviu Dudau <Liviu.Dudau@arm.com>
    CC: Lorenzo Pieralisi <Lorenzo.Pieralisi@arm.com>
    CC: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/pci.c
+++ arch/arm64/kernel/pci.c
@@ -10,7 +10,6 @@
  *
  */
 
-#include <linux/acpi.h>
 #include <linux/init.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
@@ -60,12 +59,3 @@ int pcibios_add_device(struct pci_dev *dev)
 
 	return 0;
 }
-
-#ifdef CONFIG_ACPI
-/* Root bridge scanning */
-struct pci_bus *pci_acpi_scan_root(struct acpi_pci_root *root)
-{
-	/* TODO: Should be revisited when implementing PCI on ACPI */
-	return NULL;
-}
-#endif
