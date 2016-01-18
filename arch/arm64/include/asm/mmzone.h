e8d4922da6f6b36882fdd9729a9e52ccccecc74d
Ganapatrao Kulkarni <gkulkarni@caviumnetworks.com>
2016-01-18 22:06:00 +0530

    arm64, numa: adding numa support for arm64 platforms.
    Adding numa support for arm64 based platforms.
    This patch adds by default the dummy numa node and
    maps all memory and cpus to node 0.
    using this patch, numa can be simulated on single node arm64 platforms.
    
    Tested-by: Shannon Zhao <shannon.zhao@linaro.org>
    Reviewed-by: Robert Richter <rrichter@cavium.com>
    Signed-off-by: Ganapatrao Kulkarni <gkulkarni@caviumnetworks.com>

---
+++ arch/arm64/include/asm/mmzone.h
@@ -0,0 +1,18 @@
+#ifndef __ASM_MMZONE_H
+#define __ASM_MMZONE_H
+
+#ifdef CONFIG_NUMA
+
+#include <linux/mmdebug.h>
+#include <linux/types.h>
+
+#include <asm/smp.h>
+#include <asm/numa.h>
+
+extern struct pglist_data *node_data[];
+
+#define NODE_DATA(nid)		(node_data[(nid)])
+
+#endif /* CONFIG_NUMA */
+
+#endif /* __ASM_MMZONE_H */
--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -453,6 +453,7 @@ void __init paging_init(void)
 	zero_page = early_alloc(PAGE_SIZE);
 
 	bootmem_init();
+	memblock_dump_all();
 
 	empty_zero_page = virt_to_page(zero_page);
 
