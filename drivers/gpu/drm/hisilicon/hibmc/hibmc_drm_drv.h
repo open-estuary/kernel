e1c510fb9a307cd3d0d270d6e37f46a223726d96
Rongrong Zou <zourongrong@gmail.com>
2016-05-29 17:40:49 +0800

    drm/hisilicon/hibmc: Add hisilicon hibmc drm master driver
    Add DRM master driver for Hisilicon Hibmc SoC which used for
    Out-of-band management. Blow is the general hardware connection,
    both the Hibmc and the host CPU are on the same mother board.
    
    +----------+       +----------+
    |          | PCIe  |  Hibmc   |
    |host CPU( |<----->| display  |
    |arm64,x86)|       |subsystem |
    +----------+       +----------+
    
    Signed-off-by: Rongrong Zou <zourongrong@gmail.com>
    Signed-off-by: Jianhua Li <lijianhua@huawei.com>

---
+++ drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.h
@@ -0,0 +1,39 @@
+/* Hisilicon Hibmc SoC drm driver
+ *
+ * Based on the bochs drm driver.
+ *
+ * Copyright (c) 2016 Huawei Limited.
+ *
+ * Author:
+ *	Rongrong Zou <zourongrong@huawei>
+ *	Rongrong Zou <zourongrong@gmail.com>
+ *	Jianhua Li <lijianhua@huawei.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+#ifndef HIBMC_DRM_DRV_H
+#define HIBMC_DRM_DRV_H
+
+#include <drm/drmP.h>
+#include <drm/drm_fb_helper.h>
+#include <drm/drm_gem_cma_helper.h>
+
+struct hibmc_drm_device {
+	/* hw */
+	void __iomem   *mmio;
+	void __iomem   *fb_map;
+	unsigned long  fb_base;
+	unsigned long  fb_size;
+
+	/* drm */
+	struct drm_device  *dev;
+	bool mode_config_initialized;
+};
+
+
+#endif
