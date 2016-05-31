079b3231d712dcd9b6bba146da15177ff2e9c08f
Christopher Covington <cov@codeaurora.org>
2016-05-31 16:19:02 -0400

    arm64: Workaround for QDF2432 ID_AA64 SR accesses
    The ARMv8.0 architecture reserves several system register encodings for
    future use. These encodings should behave as read-only and always return
    zero on a read. As described in Errata 94, the CPU cores in the QDF2432
    errantly cause an instruction abort upon an AArch64 read attempt to the
    following system register encodings using the MRS instruction:
    
    	Op0, Op1, CRn, CRm,        Op2
    	3,   0,   C0,  [C4-C7],    [2-3, 6-7]
    	3,   0,   C0,  C3,         [3-7]
    	3,   0,   C0,  [C4,C6,C7], [4-5]
    	3,   0,   C0,  C2,         [6-7]
    
    Naively projecting ARMv8.0 names, this space includes:
    
    	ID_AA64PFR[2-7]_EL1
    	ID_AA64DFR[2-3]_EL1
    	ID_AA64AFR[2-3]_EL1
    	ID_AA64ISAR[2-7]_EL1
    	ID_AA64MMFR[2-7]_EL1
    
    As of v4.7-rc1, Linux only attempts to query one register in this space,
    ID_AA64MMFR2_EL1. As simple workaround, skip that access when the affected
    MIDR is detected.
    
    Change-Id: I0d31c99215c5ffbc0035972bb9cf3d3dbcb3ff3a
    Signed-off-by: Christopher Covington <cov@codeaurora.org>
    Signed-off-by: Shanker Donthineni <shankerd@codeaurora.org>

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -325,6 +325,9 @@ static void cpuinfo_detect_icache_policy(struct cpuinfo_arm64 *info)
 
 static void __cpuinfo_store_cpu(struct cpuinfo_arm64 *info)
 {
+	bool id_aa64_fix = read_cpuid_implementor() == ARM_CPU_IMP_QCOM &&
+				read_cpuid_part_number() == QCOM_CPU_PART_KRYO;
+
 	info->reg_cntfrq = arch_timer_get_cntfrq();
 	info->reg_ctr = read_cpuid_cachetype();
 	info->reg_dczid = read_cpuid(DCZID_EL0);
@@ -337,7 +340,7 @@ static void __cpuinfo_store_cpu(struct cpuinfo_arm64 *info)
 	info->reg_id_aa64isar1 = read_cpuid(ID_AA64ISAR1_EL1);
 	info->reg_id_aa64mmfr0 = read_cpuid(ID_AA64MMFR0_EL1);
 	info->reg_id_aa64mmfr1 = read_cpuid(ID_AA64MMFR1_EL1);
-	info->reg_id_aa64mmfr2 = read_cpuid(ID_AA64MMFR2_EL1);
+	info->reg_id_aa64mmfr2 = id_aa64_fix ? 0 : read_cpuid(ID_AA64MMFR2_EL1);
 	info->reg_id_aa64pfr0 = read_cpuid(ID_AA64PFR0_EL1);
 	info->reg_id_aa64pfr1 = read_cpuid(ID_AA64PFR1_EL1);
 
