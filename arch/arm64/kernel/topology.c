3f5c0397b869dc49aa10bb6485444be144afe063
Timmy Li <scuttimmy@qq.com>
2016-11-26 13:24:20 +0800

    arm64: CPU topology debug info restore

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -268,11 +268,11 @@ void store_cpu_topology(unsigned int cpuid)
 					 MPIDR_AFFINITY_LEVEL(mpidr, 3) << 16;
 	}
 
-topology_populated:
-	pr_info("CPU%u: cluster %d core %d thread %d mpidr %#016llx\n",
+	pr_debug("CPU%u: cluster %d core %d thread %d mpidr %#016llx\n",
 		 cpuid, cpuid_topo->cluster_id, cpuid_topo->core_id,
 		 cpuid_topo->thread_id, mpidr);
 
+topology_populated:
 	update_siblings_masks(cpuid);
 }
 
