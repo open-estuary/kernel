6dacc6c65a958d53e5a30a4689da5c57127081a2
Anurup M <anurup.m@huawei.com>
2017-02-25 05:03:34 -0500

    Revert "arm: arm64: Add routine to determine cpuid of other cpus"
    This reverts commit 7409ee71a0377fc3458457e20f9dadb26f2ca2c0.

--- arch/arm/include/asm/cputype.h
+++ arch/arm/include/asm/cputype.h
@@ -235,8 +235,6 @@ static inline unsigned int __attribute_const__ read_cpuid_mpidr(void)
 #define cpu_is_sa1100() (read_cpuid_part() == ARM_CPU_PART_SA1100)
 #define cpu_is_sa1110() (read_cpuid_part() == ARM_CPU_PART_SA1110)
 
-#define read_specific_cpuid(cpu_num) per_cpu_ptr(&cpu_data, cpu_num)->cpuid
-
 /*
  * Intel's XScale3 core supports some v6 features (supersections, L2)
  * but advertises itself as v5 as it does not support the v6 ISA.  For
