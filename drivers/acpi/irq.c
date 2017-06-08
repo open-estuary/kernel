a84429a1deb1d27b947c3ddbc6e06c931014134e
Graeme Gregory <graeme.gregory@linaro.org>
2017-06-08 22:21:53 +0100

    HACK: acpi:irq : some people cant get producer/consumer flags right
    See https://bugzilla.redhat.com/show_bug.cgi?id=1285107#c34 for source
    of the patch.
    
    Signed-off-by: Mark Salter <msalter@redhat.com>
    Signed-off-by: Graeme Gregory <graeme.gregory@linaro.org>

--- drivers/acpi/irq.c
+++ drivers/acpi/irq.c
@@ -200,8 +200,6 @@ static acpi_status acpi_irq_parse_one_cb(struct acpi_resource *ares,
 		return AE_CTRL_TERMINATE;
 	case ACPI_RESOURCE_TYPE_EXTENDED_IRQ:
 		eirq = &ares->data.extended_irq;
-		if (eirq->producer_consumer == ACPI_PRODUCER)
-			return AE_OK;
 		if (ctx->index >= eirq->interrupt_count) {
 			ctx->index -= eirq->interrupt_count;
 			return AE_OK;
